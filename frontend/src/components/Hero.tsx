fb493dbe805324b9b37371cde829c898bd70ee5d
alhailrose <sfqbgs77@gmail.com>
2024-02-28 15:34:20 +0700

    adding layout

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <div className='bg-red-400 pb-16'>
+      <div className='container mx-auto flex flex-col gap-2'>
+        <h1 className='text-5xl text-white font-bold'>
+          Kalian mau cari Kos Kosan ?
+        </h1>
+        <p className='text-2xl text-white'>
+          Cari kos kosan murah hanya ada disini (ada yang bebas loh)
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
